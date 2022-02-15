baba00e834e8b67da967b91366a7edd232ce15b4
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2022-02-16 01:50:52 +0330

    Add files via upload

---
+++ rest-sample.php
@@ -0,0 +1,19 @@
+<?php
+
+    require('lib/rest.php');
+
+    // SSL enabled version, use it after curl SSL configuration
+    // $restClient = new RestClient('username', 'password', true);
+
+    $restClient = new RestClient('username', 'password');
+
+    $result = $restClient->SendSMS('09123456789', '5000xxx', 'test sms', false);
+    // $result = $restClient->GetDeliveries2(66666);
+    // $result = $restClient->GetMessages(1, '5000xxx', 0, 100);
+    // $result = $restClient->GetCredit();
+    // $result = $restClient->GetUserNumbers();
+    
+    print_r($result);
+    // echo $result['Value'];   // get specific member value
+
+?>
