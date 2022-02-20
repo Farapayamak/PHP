b664bbdf8f4e18375a8d73d422a1fd9af02752fc
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2022-02-20 11:31:05 +0330

    Update soap-sample.php

--- soap-sample.php
+++ soap-sample.php
@@ -2,7 +2,7 @@
 
 require('lib/soap.php');
 
-$soapClient = new Soap_Client('apitest1400', 'api@234556@@');
+$soapClient = new Soap_Client('username', 'password');
 
 // print_r($soapClient->GetCredit());
 echo $soapClient->GetCredit()->GetCreditResult;
