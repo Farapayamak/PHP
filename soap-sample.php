b7f8221d69659d5c832d497c634df5176069aeb9
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2022-02-26 01:51:13 +0330

    Update soap-sample.php

--- soap-sample.php
+++ soap-sample.php
@@ -2,7 +2,7 @@
 
 require('lib/soap.php');
 
-$soapClient = new Soap_Client('username', 'password');
+$soapClient = new Farapayamak\Soap_Client('username', 'password');
 
 // print_r($soapClient->GetCredit());
 echo $soapClient->GetCredit()->GetCreditResult;
