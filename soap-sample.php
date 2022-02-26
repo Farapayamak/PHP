e836bb8a98a0b8887c8f7ea65902633ca61d28b1
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2022-02-26 14:48:23 +0330

    Update soap-sample.php

--- soap-sample.php
+++ soap-sample.php
@@ -5,8 +5,8 @@ require('lib/Soap_Client.php');
 $soapClient = new Farapayamak\Soap_Client('username', 'password');
 
 // print_r($soapClient->GetCredit());
-echo $soapClient->GetCredit()->GetCreditResult;
-
+// echo $soapClient->GetCredit()->GetCreditResult;
+print_r($soapClient->SendSimpleSMS2('09123456789', '5000xxx', 'test sms', false));
 
 
 ?>
